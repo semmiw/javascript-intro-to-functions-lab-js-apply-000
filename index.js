40075b67b1c03c0af3bf0f741f8b92bd6d58221d
Semhar Woldeyesus <semmi.semmiw@gmail.com>
2018-11-02 11:59:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) {
+  it('hello', function() {
+  return
+  string.toUpperCase 
+}
+
+  it('receives one argument and returns it in all caps', function() {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})
